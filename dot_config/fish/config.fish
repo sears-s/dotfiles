67489549352564cba1501ebcb85cb3540742bef9
Sears Schulz <36250748+sears-s@users.noreply.github.com>
2023-04-20 10:53:33 -0500

    Install fisher if not already installed

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,11 +1,15 @@
 # Set default editor to Neovim
 set -gx EDITOR nvim
 
-# Use Ctrl+F instead of Ctrl+Alt+F for fzf file shortcut
-fzf_configure_bindings --directory=\cf
-
 # Commands to run in interactive sessions can go here
 if status is-interactive
+
+	# Install fisher if needed
+	if not type -q fisher
+		curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+		fisher update
+		echo 3 1 2 1 1 1 2 2 4 1 2 2 y | tide configure >/dev/null
+	end
 
 	# Dynamically change Tide OS icon
 	# Considerable slow down: https://github.com/IlanCosman/tide/issues/236
@@ -13,6 +17,12 @@ if status is-interactive
 	set -U tide_os_icon $os_branding_icon
 	set -U tide_os_color $os_branding_color
 	set -U tide_os_bg_color $os_branding_bg_color
+
+	# Add path
+	fish_add_path ~/.local/bin
+
+	# Use Ctrl+F instead of Ctrl+Alt+F for fzf file shortcut
+	fzf_configure_bindings --directory=\cf
 
 	# Abbreviations
 	if type -q bat
