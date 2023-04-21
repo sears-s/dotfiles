c2f79f58876aa0bb370d7557d8806be1b3a6e5fd
Sears Schulz <36250748+sears-s@users.noreply.github.com>
2023-04-21 10:14:32 -0500

    Use abbreviations that support sudo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,11 @@
 # Set default editor to Neovim
 set -gx EDITOR nvim
 
+# Set ripgrep config path
+if type -q rg
+	set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep
+end
+
 # Commands to run in interactive sessions can go here
 if status is-interactive
 
@@ -10,26 +15,19 @@ if status is-interactive
 	# Set theme
 	fish_config theme choose Dracula
 
-	# Install fisher if needed
-	if not type -q fisher
-		curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-		fisher update
-		echo 3 1 2 1 1 1 2 2 4 1 2 2 y | tide configure >/dev/null
-	end
-
 	# Use Ctrl+F instead of Ctrl+Alt+F for fzf file shortcut
 	# Leave Ctrl+R to atuin
 	fzf_configure_bindings --directory=\cf --history=
 
 	# Dynamically change Tide OS icon
 	# Considerable slow down: https://github.com/IlanCosman/tide/issues/236
-	function fix_os_icon
+	function _tide_fix_os_icon
 		_tide_detect_os | read -g --line os_branding_icon os_branding_color os_branding_bg_color
 		set -U tide_os_icon $os_branding_icon
 		set -U tide_os_color $os_branding_color
 		set -U tide_os_bg_color $os_branding_bg_color
 	end
-	fix_os_icon
+	_tide_fix_os_icon
 
 	# Add path
 	fish_add_path ~/.local/bin
@@ -41,7 +39,7 @@ if status is-interactive
 			atuin gen-completions --shell fish | source
 		else
 			echo "Warning: not logged into atuin account"
-			echo "atuin login -u <USERNAME> -p <PASSWORD>"
+			echo "atuin login -u <USERNAME> -p '<PASSWORD>' -k '<KEY>'"
 		end
 	else
 		echo "Warning: atuin not installed"
@@ -52,28 +50,48 @@ if status is-interactive
 		chezmoi completion fish | source
 	end
 
-	# Set ripgrep config path
-	if type -q rg
-		set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep
+	# Add abbreviations that support sudo
+	function _abbr_func
+		set -l old_cmd $argv[1]
+		set -l new_cmd $argv[2]
+		set -l cmd (commandline -op)
+		if [ (count $cmd) -eq 1 -a "$cmd[1]" = $old_cmd ] || \
+		[ (count $cmd) -eq 2 -a "$cmd[1]" = sudo -a "$cmd[2]" = $old_cmd ]
+			echo $new_cmd
+			return 0
+		end
+		return 1
+	end
+	function _add_abbr
+		abbr -a $argv[1] --position anywhere -f _abbr_$argv[1]
 	end
 
-	# Aliases
-	function add_alias
-		if type -q $argv[2]
-			alias $argv[1]="$argv[2]"
-		end
+	# cat -> bat
+	if type -q bat
+		function _abbr_cat; _abbr_func cat bat; end
+		_add_abbr cat
 	end
-	add_alias cat bat
-	add_alias vi nvim
-	add_alias vim nvim
-	add_alias grep rg
+
+	# vi/vim -> nvim
+	if type -q nvim
+		function _abbr_vi; _abbr_func vi nvim; end
+		_add_abbr vi
+		function _abbr_vim; _abbr_func vim nvim; end
+		_add_abbr vim
+	end
+
+	# grep -> rg
+	if type -q rg
+		function _abbr_grep; _abbr_func grep rg; end
+		_add_abbr grep
+	end
 
 	# Wrapper for distrobox-enter -r
 	# Fixes OS icon not restoring
 	if type -q distrobox-enter
 		function dbe -w distrobox-enter
 			distrobox-enter -r $argv
-			fix_os_icon
+			_tide_fix_os_icon
 		end
 	end
 
